f230181bbb10505fa5f3984da35e929a0c59a24a
Mariano Rodriguez <merodriguez81@gmail.com>
2026-01-16 20:08:34 -0300

    Add support for Articles

---
+++ Models/ArticleDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace func_mrodriguez_portfolio.Models
+{
+    public sealed class ArticleDto
+    {
+        public string Slug { get; init; } = "";
+        public string Title { get; init; } = "";
+        public string Date { get; init; } = "";
+        public string[] Tags { get; init; } = Array.Empty<string>();
+        public string Summary { get; init; } = "";
+        public string ContentHtml { get; init; } = "";
+    }
+}
